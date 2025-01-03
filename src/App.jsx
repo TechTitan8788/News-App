22168071a13f58cebf275538a1328417a2172f5e
Tech Titans <lokhandeVishal8788@gmail.com>
2025-01-03 03:58:36 +0000

    add boostrp , navbar , news , newsitem jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-class App extends Component {
-  c = "Vishal Lokhande"; // Class property
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import NewsItem from "./components/NewsItem";
 
+class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World {this.c}</h1>
+        <Navbar />
+        <News />
+        <NewsItem />
       </div>
     );
   }
---
+++ src/components/News.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+  render() {
+    return(
+        <div>
+            <h1>News</h1>
+        
+        </div>  
+    )
+}
+}
+
+export default News;
